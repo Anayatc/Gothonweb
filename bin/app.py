cd8afa4d868022ffceb27a97719325c4852ffb86
anayatc@gmail.com <anayatc@sky.com>
2017-05-07 12:03:35 +0100

    taking user input from web

--- bin/app.py
+++ bin/app.py
@@ -1,13 +1,13 @@
 import web
 
 urls = (
-  '/', 'Index'
+  '/hello', 'Index'
 )
+
 
 app = web.application(urls, globals())
 
 render = web.template.render('templates/')
-
 
 class Index(object):
     def GET(self):
